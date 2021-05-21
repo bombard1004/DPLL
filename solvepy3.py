9f57d89730d73a8fbfb20bba91bdfaeaedf723b0
Kyeongmin Choi <bombard1004@gmail.com>
2021-05-21 09:32:34 +0900

    class defs

--- solvepy3.py
+++ solvepy3.py
@@ -1,7 +1,67 @@
 import sys
+from typing import List
 
-# clause = (vars, assignedvars, neg, conflict)
-# cluases = (unit, incomplete, complete, conflict)
+class Assignment():
+    '''
+    Assignment = (var, val, isImplied)
+    '''
+    def __init__(self, var: int, val: bool, isImplied: bool) -> None:
+        self.var: int = var
+        self.val: bool = val
+        self.isImplied: bool = isImplied
+
+class Clause():
+    '''
+    Clause = (vars, assignedvars, neg, conflict)
+    '''
+    def __init__(self, vars: List[int], asms: List[Assignment]) -> None:
+        self.variableBits: int = 0
+        self.remainingBits: int = 0
+        self.negativenessBits: int = 0
+        self.conflictnessBits: int = 0
+
+        for var in vars:
+            if var < 0:
+                self.negativenessBits |= (1 << (-var-1))
+            self.variableBits |= (1 << (abs(var)-1))
+        
+        self.remainingBits = self.variableBits
+        for asm in asms:
+            if self.contains(asm.var):
+                varLoc: int = (1 << (asm.var-1))
+                self.remainingBits ^= varLoc
+                if bool(self.negativeness & varLoc) != asm.val:
+                    self.conflictnessBits |= varLoc
+    
+    def contains(self, var: int) -> bool:
+        return bool(self.variableBits & (1 << (var-1)))
+    
+    def remains(self, var: int) -> bool:
+        return bool(self.remainingBits & (1 << (var-1)))
+    
+    def isUnit(self) -> bool:
+        return not (self.remainingBits & (self.remainingBits - 1))
+
+class Formula():
+    '''
+    Formula = (unit, incomplete, complete, conflict)
+    '''
+    def __init__(self) -> None:
+        self.units: List[Clause] = []
+        self.incompletes: List[Clause] = []
+        self.completes: List[Clause] = []
+        self.conflicts: List[Clause] = []
+    
+    def addClause(self, clause: Clause) -> None:
+        if clause.conflictnessBits:
+            self.conflicts.append(clause)
+        elif clause.remainingBits == 0:
+            self.completes.append(clause)
+        elif clause.isUnit():
+            self.units.append(clause)
+        else:
+            self.incompletes.append(clause)
+
 
 def parse(filename):
     with open(filename, 'r') as f:
@@ -11,29 +71,27 @@ def parse(filename):
                 nbvar, nbclauses = map(int, line.split()[2:4])
                 break
         
-        clauses = list()
+        formula = Formula()
         for i in range(nbclauses):
-            clause = [0] * nbvar
-            for var in map(int, f.readline().split()[:-1]):
-                clause[abs(var)-1] = 1 if var > 0 else -1
-            clauses.append(clause)
+            clause = Clause(map(int, f.readline().split()[:-1]), [])
+            formula.addClause(clause)
     
-    return nbvar, nbclauses, clauses
+    return nbvar, nbclauses, formula
 
-def DPLL(nbvar, nbclauses, clauses):
+def DPLL(nbvar, nbclauses, formula):
     A = list()
 
     while True:
 
         while True:
-            unit = getUnit(clauses)
+            unit = getUnit(formula)
             if unit is None: break
             assignUnit(unit, A)
         
         if isEmpty(clauses):
             return A
         
-        if hasConflict(clauses):
+        if hasConflict(formula):
             C = learningClause()
             if(isEmpty(C)):
                 return None
@@ -45,8 +103,8 @@ def DPLL(nbvar, nbclauses, clauses):
         
 
 def main(filename):
-    nbvar, nbclauses, clauses = parse(filename)
-    res = DPLL(nbvar, nbclauses, clauses)
+    nbvar, nbclauses, formula = parse(filename)
+    res = DPLL(nbvar, nbclauses, formula)
     makeOutput(res)
 
 if __name__ == '__main__':
